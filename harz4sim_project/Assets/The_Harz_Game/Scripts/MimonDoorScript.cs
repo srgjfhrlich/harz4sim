ee08d52867bfb4bef2d508428c74367a0b35dcf4
staley88 <68488464+staley88@users.noreply.github.com>
2020-07-27 22:07:23 +0200

    Mimon Haus
    Stinktier Tür Script und Animation

---
+++ harz4sim_project/Assets/The_Harz_Game/Scripts/MimonDoorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MimonDoorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator doorAnim;
+
+    void Start()
+    {
+        doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            doorAnim.SetBool("PressedE", true);
+            SceneManager.LoadScene("MimonsHaus");
+        }
+    }
+
+   
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorAnim.SetBool("PressedE", false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
